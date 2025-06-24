641e935350f1c387b71573aef6f63c902902a7ea
Zane Shannon <z@zcs.me>
2025-06-24 15:45:57 -0700

    setup gomobile binding. add swift package

---
+++ Makefile
@@ -0,0 +1,10 @@
+build:
+	gomobile bind -target=ios,iossimulator,macos -o Sources/TSGoBindings.xcframework github.com/microsoft/typescript-go/bridge
+
+setup:
+	go get -d golang.org/x/mobile/cmd/gomobile
+	go install golang.org/x/mobile/cmd/gomobile
+	gomobile init
+
+test:
+	@cd lib/esbuildmobile && go test
---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:6.1
+import PackageDescription
+
+let package = Package(
+    name: "SwiftTSGo",
+    platforms: [.iOS(.v17), .macOS(.v14)],
+    products: [
+        .library(
+            name: "SwiftTSGo",
+            targets: ["SwiftTSGo"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SwiftTSGoMobile",
+            path: "./Sources/TSGoBindings.xcframework"
+        ),
+        .target(
+            name: "SwiftTSGo",
+            dependencies: [
+                .target(name: "SwiftTSGoMobile")
+                // .byName(name: "ESBuildMobile")
+            ]
+        ),
+        // .testTarget(
+        //     name: "ESBuildMobileTests",
+        //     dependencies: [
+        //         .target(name: "ESBuild"),
+        //         .target(name: "ESBuildMobile"),
+        //     ]
+        // ),
+    ]
+)
