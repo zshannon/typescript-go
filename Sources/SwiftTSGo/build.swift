bfbbb510f346644f05962d52de9d7eccf051d45c
Zane Shannon <z@zcs.me>
2025-06-24 16:34:42 -0700

    add test for error. swift-format

--- Sources/SwiftTSGo/build.swift
+++ Sources/SwiftTSGo/build.swift
@@ -1,12 +1,12 @@
 import TSGoBindings
 
-public func build(projectPath: String = ".") -> Bool {
+public func build(projectPath: String = ".") throws -> Bool {
     var error: NSError?
     let success = BridgeBuild(projectPath, &error)
 
     if let error = error {
         print("Build failed with error: \(error.localizedDescription)")
-        return false
+        throw error
     }
 
     return success
